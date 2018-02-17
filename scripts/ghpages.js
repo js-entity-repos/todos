f06e0d6bd939edaded153fb2896d5d83bab11a50
Ryan Smith <0ryansmith1994@gmail.com>
2018-02-17 10:54:39 +0000

    ci(circle): Attempts GH Pages CI.

--- scripts/ghpages.js
+++ scripts/ghpages.js
@@ -1,6 +1,11 @@
 var ghpages = require('gh-pages');
 
-ghpages.publish('dist', (err) => {
+ghpages.publish('dist', {
+  user: {
+    name: 'HT2 Bot',
+    email: 'hello@ht2labs.com'
+  }
+}, (err) => {
   if (err) {
     console.error(err);
     return;
