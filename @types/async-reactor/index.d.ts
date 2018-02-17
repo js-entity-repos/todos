33d04ffd3f1a8bd872a3a65e8eafbad6a57dd4e7
Ryan Smith <0ryansmith1994@gmail.com>
2018-02-17 11:08:06 +0000

    fix(deps): Removes unused async-reactor.

--- @types/async-reactor/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'async-reactor' {
-  import { Component } from "react";
-  export const asyncReactor: <P, C>(
-    component: (props: P) => Promise<C>,
-    loader?: Component,
-    error?: Component
-  ) => (props: P) => C;
-}
