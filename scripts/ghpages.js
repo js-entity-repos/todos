1f7dd42e88f79cec0ca4aeaaa1298e8b6c83e190
Ryan Smith <0ryansmith1994@gmail.com>
2018-02-17 10:52:27 +0000

    ci(circle): Attempts GH Pages CI.

--- scripts/ghpages.js
+++ scripts/ghpages.js
@@ -1,3 +1,9 @@
 var ghpages = require('gh-pages');
 
-ghpages.publish('dist');
+ghpages.publish('dist', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Published to GH Pages');
+});
